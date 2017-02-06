b944e04f8d8cddfde06be2f6bf8625dc7a92c441
Elliott Bolzan <elliott.bolzan@duke.edu>
2017-02-05 23:11:54 -0500

    Modified Main.

--- src/cellsociety_team18/Main.java
+++ src/cellsociety_team18/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	 */
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		ViewController controller = new ViewController(primaryStage);
+		new ViewController(primaryStage);
 	}
 
 	/**
